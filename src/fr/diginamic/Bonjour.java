97fc64ddae56d1dedce303c07a3683f439655aad
popobg <pauline.bouyssou1@gmail.com>
2024-10-02 11:32:19 +0200

    tp 01: first commit

---
+++ src/fr/diginamic/Bonjour.java
@@ -0,0 +1,8 @@
+package fr.diginamic;
+
+public class Bonjour {
+    public static void main(String[] args) {
+        // "sout" for quick completion by IntelliJ for print in console
+        System.out.println("Bonjour tout le monde !");
+    }
+}
